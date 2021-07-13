6881b9ce5d11349d087eb7fd77ab4a40a8c091d0
CodeKartik <kartik.more2802@gmail.com>
2021-07-14 01:23:19 +0530

    Added font style and size persistance

--- lib/main.dart
+++ lib/main.dart
@@ -10,18 +10,22 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SharedPreferences pref = await SharedPreferences.getInstance();
   SystemChrome.setPreferredOrientations(
-      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
-
-  runApp(ChangeNotifierProvider(
-    create: (context) => CartModel(),
-    child: GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(fontFamily: 'SF'),
-      title: 'Apple Store',
-      color: Colors.white,
-      home: SplashScreen(
-        pref: pref,
-      ),
-    ),
-  ));
+          [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown])
+      .then((value) => runApp(ChangeNotifierProvider(
+            create: (context) => CartModel(),
+            child: GetMaterialApp(
+              builder: (context, child) {
+                return MediaQuery(
+                    data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+                    child: child!);
+              },
+              debugShowCheckedModeBanner: false,
+              theme: ThemeData(fontFamily: 'SF'),
+              title: 'Apple Store',
+              color: Colors.white,
+              home: SplashScreen(
+                pref: pref,
+              ),
+            ),
+          )));
 }
